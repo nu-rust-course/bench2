244fce90e528c05e5882ce0b6ca2fcb66845ff03
Jesse A. Tov <jesse.tov@gmail.com>
2018-04-29 13:00:41 -0500

    Runs benchmark.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,23 @@
 extern crate bench2;
 
-use bench2::{Bench2, /*secs_micros::SecsMicros*/};
+use bench2::{Bench2, secs_micros::SecsMicros};
 
 use std::env::args;
 
 fn main() {
-    let _relayed_argument = args().nth(1)
+    let relayed_argument = args().nth(1)
         .expect(&format!("Usage: {} ARGUMENT", prog_name()));
 
     let mut bench = Bench2::new();
+    bench.arg(relayed_argument);
     bench.run_iters(8);
+    bench.add_input_str("h\nhe\nhel\nhell\nhello\nhelloo\nhellooo\n");
 
     bench.build_release().unwrap();
 
-//    let timing = run_with_arg_and_input(&relayed_argument, "");
+    let timing = bench.time_subject().unwrap();
 
-//    println!("{}", SecsMicros(timing));
+    println!("{}", SecsMicros(timing));
 }
 
 fn prog_name() -> String {
