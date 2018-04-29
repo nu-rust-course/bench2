aeb3574c209dd0744cb3364f55e72b7cb04f6562
Jesse A. Tov <jesse.tov@gmail.com>
2018-04-29 17:55:07 -0500

    Added description. Relaying all arguments.

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,11 @@ use bench2::{Bench2, secs_micros::SecsMicros};
 use std::env::args;
 
 fn main() {
-    let relayed_argument = args().nth(1)
-        .expect(&format!("Usage: {} ARGUMENT", prog_name()));
+    let mut args = args();
+    args.next();
 
     let mut bench = Bench2::new();
-    bench.arg(relayed_argument);
+    bench.args(args);
     bench.run_iters(8);
     bench.add_input_str("h\nhe\nhel\nhell\nhello\nhelloo\nhellooo\n");
 
@@ -20,6 +20,3 @@ fn main() {
     println!("{}", SecsMicros(timing));
 }
 
-fn prog_name() -> String {
-    args().next().unwrap_or_else(|| "bench2".to_owned())
-}
